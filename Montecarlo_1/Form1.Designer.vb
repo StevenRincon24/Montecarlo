f73a7a33962807232d0c8d2f83eeed3242379136
StevenRincon24 <74875614+StevenRincon24@users.noreply.github.com>
2023-07-29 00:58:12 -0500

    first commit

---
+++ Montecarlo_1/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea13 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend13 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series13 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea14 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend14 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series14 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea15 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend15 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series15 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea16 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend16 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series16 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.CienPuntos = New System.Windows.Forms.Button()
+        Me.LimiteInferior = New System.Windows.Forms.TextBox()
+        Me.LimiteSuperior = New System.Windows.Forms.TextBox()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Chart200 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Chart500 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Chart1000 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.PuntosDentro100 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Numero = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.y = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart200, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart500, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1000, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CienPuntos
+        '
+        Me.CienPuntos.Location = New System.Drawing.Point(123, 128)
+        Me.CienPuntos.Name = "CienPuntos"
+        Me.CienPuntos.Size = New System.Drawing.Size(75, 23)
+        Me.CienPuntos.TabIndex = 3
+        Me.CienPuntos.Text = "Calcular"
+        Me.CienPuntos.UseVisualStyleBackColor = True
+        '
+        'LimiteInferior
+        '
+        Me.LimiteInferior.Location = New System.Drawing.Point(53, 85)
+        Me.LimiteInferior.Name = "LimiteInferior"
+        Me.LimiteInferior.Size = New System.Drawing.Size(100, 20)
+        Me.LimiteInferior.TabIndex = 7
+        '
+        'LimiteSuperior
+        '
+        Me.LimiteSuperior.Location = New System.Drawing.Point(171, 85)
+        Me.LimiteSuperior.Name = "LimiteSuperior"
+        Me.LimiteSuperior.Size = New System.Drawing.Size(100, 20)
+        Me.LimiteSuperior.TabIndex = 8
+        '
+        'Chart1
+        '
+        ChartArea13.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea13)
+        Legend13.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend13)
+        Me.Chart1.Location = New System.Drawing.Point(307, 29)
+        Me.Chart1.Name = "Chart1"
+        Series13.ChartArea = "ChartArea1"
+        Series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series13.Legend = "Legend1"
+        Series13.Name = "Series1"
+        Me.Chart1.Series.Add(Series13)
+        Me.Chart1.Size = New System.Drawing.Size(464, 300)
+        Me.Chart1.TabIndex = 9
+        Me.Chart1.Text = "Chart1"
+        '
+        'Chart200
+        '
+        ChartArea14.Name = "ChartArea1"
+        Me.Chart200.ChartAreas.Add(ChartArea14)
+        Legend14.Name = "Legend1"
+        Me.Chart200.Legends.Add(Legend14)
+        Me.Chart200.Location = New System.Drawing.Point(777, 29)
+        Me.Chart200.Name = "Chart200"
+        Series14.ChartArea = "ChartArea1"
+        Series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series14.Legend = "Legend1"
+        Series14.Name = "Series2"
+        Me.Chart200.Series.Add(Series14)
+        Me.Chart200.Size = New System.Drawing.Size(447, 300)
+        Me.Chart200.TabIndex = 10
+        Me.Chart200.Text = "Chart2"
+        '
+        'Chart500
+        '
+        ChartArea15.Name = "ChartArea1"
+        Me.Chart500.ChartAreas.Add(ChartArea15)
+        Legend15.Name = "Legend1"
+        Me.Chart500.Legends.Add(Legend15)
+        Me.Chart500.Location = New System.Drawing.Point(307, 337)
+        Me.Chart500.Name = "Chart500"
+        Series15.ChartArea = "ChartArea1"
+        Series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series15.Legend = "Legend1"
+        Series15.Name = "Series1"
+        Me.Chart500.Series.Add(Series15)
+        Me.Chart500.Size = New System.Drawing.Size(464, 311)
+        Me.Chart500.TabIndex = 11
+        Me.Chart500.Text = "Chart3"
+        '
+        'Chart1000
+        '
+        ChartArea16.Name = "ChartArea1"
+        Me.Chart1000.ChartAreas.Add(ChartArea16)
+        Legend16.Name = "Legend1"
+        Me.Chart1000.Legends.Add(Legend16)
+        Me.Chart1000.Location = New System.Drawing.Point(777, 337)
+        Me.Chart1000.Name = "Chart1000"
+        Series16.ChartArea = "ChartArea1"
+        Series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series16.Legend = "Legend1"
+        Series16.Name = "Series1"
+        Me.Chart1000.Series.Add(Series16)
+        Me.Chart1000.Size = New System.Drawing.Size(447, 311)
+        Me.Chart1000.TabIndex = 12
+        Me.Chart1000.Text = "Chart4"
+        '
+        'PuntosDentro100
+        '
+        Me.PuntosDentro100.AutoSize = True
+        Me.PuntosDentro100.Location = New System.Drawing.Point(50, 194)
+        Me.PuntosDentro100.Name = "PuntosDentro100"
+        Me.PuntosDentro100.Size = New System.Drawing.Size(76, 13)
+        Me.PuntosDentro100.TabIndex = 13
+        Me.PuntosDentro100.Text = "Puntos dentro:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Numero, Me.y})
+        Me.DataGridView1.Location = New System.Drawing.Point(31, 246)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'Numero
+        '
+        Me.Numero.HeaderText = "x"
+        Me.Numero.Name = "Numero"
+        '
+        'y
+        '
+        Me.y.HeaderText = "y"
+        Me.y.Name = "y"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1246, 649)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PuntosDentro100)
+        Me.Controls.Add(Me.Chart1000)
+        Me.Controls.Add(Me.Chart500)
+        Me.Controls.Add(Me.Chart200)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.LimiteSuperior)
+        Me.Controls.Add(Me.LimiteInferior)
+        Me.Controls.Add(Me.CienPuntos)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart200, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart500, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1000, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CienPuntos As Button
+    Friend WithEvents LimiteInferior As TextBox
+    Friend WithEvents LimiteSuperior As TextBox
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Chart200 As DataVisualization.Charting.Chart
+    Friend WithEvents Chart500 As DataVisualization.Charting.Chart
+    Friend WithEvents Chart1000 As DataVisualization.Charting.Chart
+    Friend WithEvents PuntosDentro100 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Numero As DataGridViewTextBoxColumn
+    Friend WithEvents y As DataGridViewTextBoxColumn
+End Class
---
+++ Montecarlo_1/Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+    Dim rnd As New Random()
+
+    Private Sub CienPuntos_Click(sender As Object, e As EventArgs) Handles CienPuntos.Click
+
+        Dim cantidadPuntosDentro100, cantidadPuntosFuera100 As Integer
+        Dim cantidadPuntosDentro200, cantidadPuntosFuera200 As Integer
+        Dim cantidadPuntosDentro500, cantidadPuntosFuera500 As Integer
+        Dim cantidadPuntosDentro1000, cantidadPuntosFuera1000 As Integer
+
+        Dim limiteInferior1 As Double = Double.Parse(LimiteInferior.Text)
+        Dim limiteSuperior1 As Double = Double.Parse(LimiteSuperior.Text)
+
+        ' Limpiar las series anteriores (opcional)
+        Chart1.Series.Clear()
+
+        ' Crear una nueva serie de datos para la función original
+        Dim seriesFuncion As New DataVisualization.Charting.Series()
+        seriesFuncion.Name = "Valor Absoluto de x"
+        seriesFuncion.ChartType = DataVisualization.Charting.SeriesChartType.Line
+
+        ' Crear una nueva serie de datos para los puntos aleatorios
+        Dim seriesAleatorios As New DataVisualization.Charting.Series()
+        seriesAleatorios.Name = "Puntos Aleatorios"
+        seriesAleatorios.ChartType = DataVisualization.Charting.SeriesChartType.Point
+
+        Dim seriesAleatorios200 As New DataVisualization.Charting.Series()
+        seriesAleatorios200.Name = "Puntos Aleatorios 200"
+        seriesAleatorios200.ChartType = DataVisualization.Charting.SeriesChartType.Point
+
+        Dim seriesAleatorios500 As New DataVisualization.Charting.Series()
+        seriesAleatorios500.Name = "Puntos Aleatorios 500"
+        seriesAleatorios500.ChartType = DataVisualization.Charting.SeriesChartType.Point
+
+        Dim seriesAleatorios1000 As New DataVisualization.Charting.Series()
+        seriesAleatorios1000.Name = "Puntos Aleatorios 500"
+        seriesAleatorios1000.ChartType = DataVisualization.Charting.SeriesChartType.Point
+
+        ' Calcular el paso entre cada punto para la función original
+        Dim pasoFuncion As Double = (limiteSuperior1 - limiteInferior1) / (100 - 1)
+
+        ' Calcular y agregar los puntos y sus valores del valor absoluto de x para la función original
+        Dim x As Double = limiteInferior1
+        For i As Integer = 0 To 100 - 1
+            Dim y As Double = Math.Abs(x)
+            ' Agregar el punto a la serie de la función original
+            seriesFuncion.Points.AddXY(x, y)
+            ' Incrementar el valor de x para el siguiente punto
+            x += pasoFuncion
+        Next
+
+        ' Calcular el tamaño del cuadrado
+        Dim ladoCuadrado As Double = Math.Abs(limiteSuperior1) + Math.Abs(limiteInferior1)
+
+        ' Generar 100 puntos aleatorios dentro del cuadrado y agregarlos a la serie de puntos aleatorios
+        For i As Integer = 1 To 10
+            ' Generar coordenadas x e y aleatorias dentro del rango
+            Dim xAleatorio As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+            Dim yAleatorio As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+            Console.WriteLine("H")
+            DataGridView1.Rows.Add(xAleatorio, yAleatorio)
+
+
+            ' Verificar si el punto aleatorio está debajo de la curva de la función
+            If yAleatorio < Math.Abs(xAleatorio) Then
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de rojo
+                seriesAleatorios.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio, yAleatorio) With {.Color = Color.Red})
+                ' Sumar uno a la cantidad de puntos que se encuentran bajo el area
+                cantidadPuntosDentro100 += 1
+
+            Else
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de azul
+                seriesAleatorios.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio, yAleatorio) With {.Color = Color.Blue})
+                ' Sumar uno a la cantidad de puntos que se encuentran por encima de la gráfica
+                cantidadPuntosFuera100 += 1
+            End If
+        Next
+
+        ' Agregar ambas series al Chart
+        Chart1.Series.Add(seriesFuncion)
+        Chart1.Series.Add(seriesAleatorios)
+        PuntosDentro100.Text = "La cantidad de puntos son: " & cantidadPuntosDentro100
+
+        For i As Integer = 1 To 200
+            ' Generar coordenadas x e y aleatorias dentro del rango
+            Dim xAleatorio200 As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+            Dim yAleatorio200 As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+
+            ' Verificar si el punto aleatorio está debajo de la curva de la función
+            If yAleatorio200 < Math.Abs(xAleatorio200) Then
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de rojo
+                seriesAleatorios200.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio200, yAleatorio200) With {.Color = Color.Red})
+                ' Sumar uno a la cantidad de puntos que se encuentran bajo el area
+                cantidadPuntosDentro200 += 1
+
+            Else
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de azul
+                seriesAleatorios200.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio200, yAleatorio200) With {.Color = Color.Blue})
+                ' Sumar uno a la cantidad de puntos que se encuentran por encima de la gráfica
+                cantidadPuntosFuera200 += 1
+            End If
+        Next
+        Chart200.Series.Add(seriesFuncion)
+        Chart200.Series.Add(seriesAleatorios200)
+
+        For i As Integer = 1 To 500
+            ' Generar coordenadas x e y aleatorias dentro del rango
+            Dim xAleatorio500 As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+            Dim yAleatorio500 As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+
+            ' Verificar si el punto aleatorio está debajo de la curva de la función
+            If yAleatorio500 < Math.Abs(xAleatorio500) Then
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de rojo
+                seriesAleatorios500.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio500, yAleatorio500) With {.Color = Color.Red})
+                ' Sumar uno a la cantidad de puntos que se encuentran bajo el area
+                cantidadPuntosDentro500 += 1
+
+            Else
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de azul
+                seriesAleatorios500.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio500, yAleatorio500) With {.Color = Color.Blue})
+                ' Sumar uno a la cantidad de puntos que se encuentran por encima de la gráfica
+                cantidadPuntosFuera500 += 1
+            End If
+        Next
+        Chart500.Series.Add(seriesFuncion)
+        Chart500.Series.Add(seriesAleatorios500)
+
+
+        For i As Integer = 1 To 1000
+            ' Generar coordenadas x e y aleatorias dentro del rango
+            Dim xAleatorio1000 As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+            Dim yAleatorio1000 As Double = limiteInferior1 + ladoCuadrado * rnd.NextDouble()
+
+            ' Verificar si el punto aleatorio está debajo de la curva de la función
+            If yAleatorio1000 < Math.Abs(xAleatorio1000) Then
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de rojo
+                seriesAleatorios500.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio1000, yAleatorio1000) With {.Color = Color.Red})
+                ' Sumar uno a la cantidad de puntos que se encuentran bajo el area
+                cantidadPuntosDentro500 += 1
+
+            Else
+                ' Agregar el punto a la serie de puntos aleatorios y pintarlo de azul
+                seriesAleatorios1000.Points.Add(New DataVisualization.Charting.DataPoint(xAleatorio1000, yAleatorio1000) With {.Color = Color.Blue})
+                ' Sumar uno a la cantidad de puntos que se encuentran por encima de la gráfica
+                cantidadPuntosFuera500 += 1
+            End If
+        Next
+        Chart1000.Series.Add(seriesFuncion)
+        Chart1000.Series.Add(seriesAleatorios1000)
+
+
+
+    End Sub
+
+End Class
